a84beac00bf3be5667a946aa7f66a2d07c3f04d7
rabby-web <rabby.webdeveloper@gmail.com>
2023-12-11 14:32:22 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-red-600 text-3xl text-center bg-yellow-400 border-2 border-red-400 p-2 m-2 rounded-lg hover:bg-slate-500">
+        Home Section
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
